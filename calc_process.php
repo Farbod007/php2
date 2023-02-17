0fe55e4f43ed0ee798b3d6ea66dd42d1b0120778
Farbod007 <123727601+Farbod007@users.noreply.github.com>
2023-02-17 09:47:17 +0100

    Add files via upload

--- calc_process.php
+++ calc_process.php
@@ -1,33 +1,36 @@
 <?php 
-
+//Deze function zorgt dat DE rekenmachine kan berekenen.
 function compute()
-    {
-        $num1 = $_POST['FirstNum'];
-        $num2 =$_POST['SecNum'];
+    { 
+        
+        $num1 = $_POST['FirstNum']; //Dit betkent $num1 is gelijk met input Firstnum 
+        $num2 =$_POST['SecNum'];//Dit betkent $num2 is gelijk met input Secnum
 
-        switch($_POST['dropdown'])
+        //De dropdown wordt gemaakt zodat je een methode kunt kiezen om te berekenen.
+        switch($_POST['dropdown']) 
         {
-            case "Addition";
+            case "Addition"; //De addition wordt gemaakt zodat je kan optellen.
               $sum = $num1 + $num2;
               return $sum;
               break;
-            case "Subtraction";
+            case "Subtraction"; //De Subtraction wordt gemaakt zodat je kan aftrekken.
               $diff = $num1 - $num2;
               return $diff;
               break;
-          case "Multiplication";
+          case "Multiplication";//De Multiplication wordt gemaakt zodat je kan vermenigvuldigen.
               $prod = $num1 * $num2;
               return $prod;
               break;
-          case "Division";
+          case "Division";//De divison wordt gemaakt zodat je kan delen.
               $pro = $num1 / $num2;
               return $pro;
               break;
-            default:
-            echo "Invalis Operation";
+            default://De default wordt gemaakt zodat je een error krijgt als je de dropdown niet gekozen had.
+            echo "Invalid Operation";
         }
     }
-    echo "the result is:". compute();
+    echo "the result is:". compute();//Deze geeft de resultaat.
     ?>
 <br>
+<!--Met deze link kan je terug naar calc.php-->
 <a href ="calc.php">back</a>
